969f815cc69d93f38dce7ca836d86793268884d8
steadylearner <32325099+steadylearner@users.noreply.github.com>
2020-12-12 09:46:21 -0300

    Include todo example. Read Haskell CLI packages documenations. Make a CLI with mongodb.

--- examples/stack/todo/app/Main.hs
+++ examples/stack/todo/app/Main.hs
@@ -1,16 +1,15 @@
 module Main where
 
--- Move this to src Lib.hs
-
 import Crud (prompt)
-
--- 
 
 main :: IO ()
 main = do
     putStrLn "Commands:"
     putStrLn "+ <String> - Add a TODO entry"
     putStrLn "- <Int>    - Delete the numbered entry"
+    putStrLn "s <Int>    - Show the numbered entry"
+    putStrLn "e <Int>    - Edit te numbered entry"
+    putStrLn "l          - List todo"
     putStrLn "q          - Quit"
     prompt []
 
--- examples/stack/todo/src/Crud.hs
+++ examples/stack/todo/src/Crud.hs
@@ -2,6 +2,9 @@ module Crud
     ( prompt
     ) where
 
+import Data.List
+-- import Data.Char (digitToInt)
+
 putTodo :: (Int, String) -> IO ()
 putTodo (n, todo) = putStrLn (show n ++ ": " ++ todo)
 
@@ -9,10 +12,15 @@ prompt :: [String] -> IO ()
 prompt todos = do
     -- print todos -- List of todo input by a user.
     putStrLn ""
-    putStrLn "Test todo with Haskell. You can use +, -, s(show), l(ist), q(uit). commands."
+    putStrLn "Test todo with Haskell. You can use +(create), -(delete), s(show), e(dit), l(ist), q(uit) commands."
     -- mapM_ putTodo (zip [0..] todos)
     command <- getLine
-    interpret command todos
+    if "e" `isPrefixOf` command
+        then do
+            print "What is the new todo for that?"
+            newTodo <- getLine
+            editTodo command todos newTodo
+        else interpret command todos
 
 -- Include update todo.
 interpret :: String -> [String] -> IO ()
@@ -20,7 +28,7 @@ interpret :: String -> [String] -> IO ()
 interpret ('+':' ':todo) todos = prompt (todo:todos) -- append todo to the empty or previous todo list [] here.
 
 interpret ('-':' ':num ) todos =
-    case delete (read num) todos of
+    case deleteOne (read num) todos of
         Nothing -> do
             putStrLn "No TODO entry matches the given number"
             prompt todos
@@ -38,7 +46,7 @@ interpret ('s':' ':num ) todos =
 interpret  "l"           todos = do
     let numberOfTodos = length todos
     putStrLn ""
-    print $ show numberOfTodos ++ " todo in total"
+    print $ show numberOfTodos ++ " in total"
 
     mapM_ putTodo (zip [0..] todos)
     prompt todos
@@ -49,15 +57,49 @@ interpret  command       todos = do
     putStrLn ("Invalid command: `" ++ command ++ "`")
     prompt todos
 
-delete :: Int -> [a] -> Maybe [a]
-delete 0 (_:as) = Just as
-delete n (a:as) = do
-    as' <- delete (n - 1) as
+deleteOne :: Int -> [a] -> Maybe [a]
+deleteOne 0 (_:as) = Just as
+deleteOne n (a:as) = do
+    as' <- deleteOne (n - 1) as
     return (a:as')
-delete _  []    = Nothing
+deleteOne _  []    = Nothing
 
 showOne :: Int -> [a] -> Maybe a
 showOne n todos = do 
-    if n > length todos
+    if (n < 0) || (n > length todos)
         then Nothing
-        else Just (todos !! n)
+        else Just (todos !! n) -- Index is not checked here? Why?
+
+editIndex :: Int -> a -> [a] -> [a]    
+editIndex i x xs = take i xs ++ [x] ++ drop (i+1) xs
+
+editTodo :: String -> [String] -> String -> IO ()
+editTodo ('e':' ':num) todos newTodo =
+    case editOne (read num) todos newTodo of
+        Nothing -> do
+            putStrLn "No TODO entry matches the given number"
+            prompt todos
+        Just todo -> do
+            putStrLn ""
+
+            print $ "Old todo is " ++ todo
+            print $ "New todo is " ++ newTodo
+            -- let index = head (map digitToInt num)
+            -- let index = read num::Int
+            -- print index
+
+            let newTodos = editIndex (read num::Int) newTodo todos -- Couldn't match expected type ‘Int’ with actual type ‘[Char]
+
+            let numberOfTodos = length newTodos
+            putStrLn ""
+            print $ show numberOfTodos ++ " in total"
+            mapM_ putTodo (zip [0..] newTodos)
+
+            prompt newTodos
+
+editOne :: Int -> [a] -> String -> Maybe a
+editOne n todos newTodo = do 
+    if (n < 0) || (n > length todos)
+        then Nothing
+        else do
+            Just (todos !! n) -- Index is not checked here? Why?
