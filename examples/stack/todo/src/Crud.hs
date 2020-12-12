bcaf36e2f6a2db74d13e558d4c5ca44b062c429d
steadylearner <32325099+steadylearner@users.noreply.github.com>
2020-12-11 21:43:51 -0300

    Include list and show todo. Should make update todo work.

--- examples/stack/todo/src/Crud.hs
+++ examples/stack/todo/src/Crud.hs
@@ -7,22 +7,44 @@ putTodo (n, todo) = putStrLn (show n ++ ": " ++ todo)
 
 prompt :: [String] -> IO ()
 prompt todos = do
+    -- print todos -- List of todo input by a user.
     putStrLn ""
-    putStrLn "Current TODO list:"
-    mapM_ putTodo (zip [0..] todos)
+    putStrLn "Test todo with Haskell. You can use +, -, s(show), l(ist), q(uit). commands."
+    -- mapM_ putTodo (zip [0..] todos)
     command <- getLine
     interpret command todos
 
--- Include update and show just one todo.
+-- Include update todo.
 interpret :: String -> [String] -> IO ()
-interpret ('+':' ':todo) todos = prompt (todo:todos)
+
+interpret ('+':' ':todo) todos = prompt (todo:todos) -- append todo to the empty or previous todo list [] here.
+
 interpret ('-':' ':num ) todos =
     case delete (read num) todos of
         Nothing -> do
             putStrLn "No TODO entry matches the given number"
             prompt todos
         Just todos' -> prompt todos'
+
+interpret ('s':' ':num ) todos =
+    case showOne (read num) todos of
+        Nothing -> do
+            putStrLn "No TODO entry matches the given number"
+            prompt todos
+        Just todo -> do
+            print $ num ++ ". " ++ todo 
+            prompt todos
+
+interpret  "l"           todos = do
+    let numberOfTodos = length todos
+    putStrLn ""
+    print $ show numberOfTodos ++ " todo in total"
+
+    mapM_ putTodo (zip [0..] todos)
+    prompt todos
+            
 interpret  "q"           todos = return ()
+
 interpret  command       todos = do
     putStrLn ("Invalid command: `" ++ command ++ "`")
     prompt todos
@@ -33,3 +55,9 @@ delete n (a:as) = do
     as' <- delete (n - 1) as
     return (a:as')
 delete _  []    = Nothing
+
+showOne :: Int -> [a] -> Maybe a
+showOne n todos = do 
+    if n > length todos
+        then Nothing
+        else Just (todos !! n)
